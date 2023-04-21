33980718ac8624428ecd088864fbd14026190267
Maysun Campos <maysuncampos@yahoo.com>
2023-04-21 12:34:40 -0600

    Theme manager

---
+++ js/themes.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var doc = document;
+
+function dark() {
+  doc.body.style = "color:white; background:black;";
+}
+
+function light() {
+  doc.body.style = "color:black; background:white;";
+}
+
+function blue() {
+  doc.body.style = "color:white; background:darkblue;";
+}
